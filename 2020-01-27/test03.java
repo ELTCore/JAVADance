d77c08dbd78029abf15084cb1394ef8c376ac99d
ELTCore <ELTCore@users.noreply.github.com>
2020-01-27 22:51:33 +0800

    2020-01-27 22:51

--- 2020-01-27/test03.java
+++ 2020-01-27/test03.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class test03{
     public static void main(String[] args) {
-        System.out.println("2020-01-07\nby HaikiYuuki\n");
-        
+        System.out.println("2020-01-27\nby HaikiYuuki\n");
+        Scanner scan = new Scanner(System.in);
+        // Get data from keyboard
 
+        // Receive the character string by 'nextLine'
+        System.out.println("Receive the character string by \"nextLine\": ");
+        
+        // Judge if there is still input
+        if (scan.hasNext()){
+            String str1 = scan.nextLine();
+            System.out.println("The string is: " + str1);
+        }
+        scan.close();
     }
 }
