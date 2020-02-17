743823098f45f626a6a0df609f54f5036d03abd6
ELTCore <ELTCore@users.noreply.github.com>
2020-02-17 23:28:06 +0800

    updata

--- 2020-02-01/test08.java
+++ 2020-02-01/test08.java
@@ -1,5 +1,7 @@
-import java.io.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class test08 {
     public static void main(String args[]) throws IOException {
         char c;
---
+++ 2020-02-16/test09.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class test09{
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Test09   by HaikiYuuki");
+        frame.setBounds(100, 100, 800, 600);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.setVisible(true);
+
+    }
+}
