47686dbff4aef1bd252bd7fe91d0c91603f3c92b
ELTCore <ELTCore@users.noreply.github.com>
2020-03-26 20:50:33 +0800

    2020-03-26

--- Lesson/week5/homework04/homework/src/homework/Student.java
+++ Lesson/week5/homework04/homework/src/homework/Student.java
@@ -3,6 +3,7 @@ package homework;
 public class Student {
     private String name = "NULL";
     private String ID = "NULL";
+    private boolean ifSet = false;
 
     public String getName() {
         return name;
@@ -19,5 +20,13 @@ public class Student {
     public void setID(String iD) {
         ID = iD;
     }
-    
+
+    public boolean isIfSet() {
+        return ifSet;
+    }
+
+    public void setIfSet(boolean ifSet) {
+        this.ifSet = ifSet;
+    }
+
 }
