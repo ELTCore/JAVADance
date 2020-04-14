cf5b6579b41cd9138cf0bc2a9e247ef94e01b56a
ELTCore <ELTCore@users.noreply.github.com>
2020-04-14 18:48:31 +0800

    Create Homework01.java

---
+++ 2020-04-14/Homework01.java
@@ -0,0 +1,9 @@
+// Write a Validate class that encapsulates the common data format
+// (user name, password, phone number, phone number, QQ number, id number,
+// postal code, Email, website, date of birth, name)
+// of the test methods, such as isMobile (), isPhone (), etc., to write a driver
+// program,
+// input the corresponding data in turn, calls the Validate class complete input
+// data inspection,
+// if the check is not passed, prompted to input,
+// until the input is correct.
