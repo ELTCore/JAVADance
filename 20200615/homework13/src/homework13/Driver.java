f0fca7292e421b416da80350b37c11e56c8e34b4
ELTCore <ELTCore@users.noreply.github.com>
2020-06-15 18:22:29 +0800

    Update Driver.java

--- 20200615/homework13/src/homework13/Driver.java
+++ 20200615/homework13/src/homework13/Driver.java
@@ -1,25 +1,24 @@
 // 新建数据库mis，建立学生表（tstudent）,各字段定义如下：
 
-// 字段名	中文含义	数据类型		 备注
-// id		学号		Varchar(20)	    主键
-// name		姓名		Varchar(20)     NOT NULL
-// gender	性别		Varchar(20)		NOT NULL
-// birth	出生日期	 DATE			NOT NULL
-// score	成绩		Decimal(9,1)	NOT NULL
+// 字段名		中文含义	数据类型		 		备注
+// id				学号			Varchar(20)		主键
+// name		姓名			Varchar(20)     	NOT NULL
+// gender		性别			Varchar(20)		NOT NULL
+// birth			出生日期	DATE					NOT NULL
+// score		成绩			Decimal(9,1)		NOT NULL
 
 // 开发Java程序，实现基本的插入和查询功能：
-
 // n 输入学生各项信息，保存到数据库
-
 // n 输入学生学号，查询显示该学号学生的所有记录
 
 package homework13;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Driver {
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost:3306/msi?useSSL=false&serverTimezone=UTC";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/mis?useSSL=false&serverTimezone=UTC";
 
 	static final String USER = "root";
 	static final String PASS = "Byzq8023";
@@ -38,10 +37,48 @@ public class Driver {
 			// 执行查询
 			System.out.println(" 实例化Statement对象...");
 			stmt = conn.createStatement();
-			String sql;
+			Scanner sc = new Scanner(System.in);
+			String sql = "";
+			int choose = 0;
+			do {
+				System.out.println("1. 插入信息");
+				System.out.println("2. 查询信息");
+				System.out.print("请选择功能: ");
+				choose = sc.nextInt();
 
-			sql = "SELECT id, name, gender, birth, score FROM tstudent";
+				if (choose == 1) {
+					String id;
+					String name;
+					String gender;
+					String birth;
+					float score;
 
+					System.out.print("\n请输入学生ID:");
+					id = sc.next();
+					System.out.print("\n请输入学生姓名:");
+					name = sc.next();
+					System.out.print("\n请输入学生性别:");
+					gender = sc.next();
+					System.out.print("\n请输入学生生日(格式:YYYY-MM-DD):");
+					birth = sc.next();
+					System.out.print("\n请输入学生成绩:");
+					score = sc.nextFloat();
+
+					sql = "INSERT INTO tstudent VALUES ('" + id + "', '" + name + "', '" + gender + "', '" + birth
+							+ "', " + score + ")";
+
+					stmt.executeUpdate(sql);
+				}
+				if (choose == 2) {
+					System.out.println("请输入查询ID: ");
+					sql = "SELECT id, name, gender, birth, score FROM tstudent WHERE id = '" + sc.next() + "'";
+				}
+			} while (choose != 1 && choose != 2);
+			sc.close();
+
+			if (choose == 1) {
+				sql = "SELECT * FROM tstudent";
+			}
 			ResultSet rs = stmt.executeQuery(sql);
 
 			// 展开结果集数据库
@@ -52,7 +89,7 @@ public class Driver {
 				String birth = rs.getString("birth");
 				float score = rs.getFloat("score");
 
-				System.out.print("ID: " + id);
+				System.out.print("\nID: " + id);
 				System.out.print(", 姓名: " + name);
 				System.out.print(", 性别: " + gender);
 				System.out.print(", 出生日期: " + birth);
@@ -76,7 +113,7 @@ public class Driver {
 			} catch (SQLException se) {
 				se.printStackTrace();
 			}
-			System.out.println("Goodbye.");
+			System.out.println("\n\nGoodbye.");
 		}
 	}
 }
